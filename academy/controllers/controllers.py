1f19dd377ab72039973b625f4becc47cacd9a167
Ardian Pramana <pramana.ardian@gmail.com>
2018-05-16 14:37:03 +0700

    [ADD] academy: Add first controller for /academy/academy

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from openerp import http
 
+
+class Academy(http.Controller):
+    @http.route('/academy/academy/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+
 # class Academy(http.Controller):
 #     @http.route('/academy/academy/', auth='public')
 #     def index(self, **kw):
